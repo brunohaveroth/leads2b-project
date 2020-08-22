d5e8cc1f7feffacfa28a491193dc6d1f7782f658
Bruno Haveroth <bruno.haveroth@gmail.com>
2020-08-22 10:00:00 -0300

    Install font-awesome

--- client/src/main.js
+++ client/src/main.js
@@ -5,9 +5,15 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import 'sweetalert2/dist/sweetalert2.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar, faEdit, faTrash, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faStar, faEdit, faTrash, faPlus)
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VueSweetalert2);
 Vue.config.productionTip = false
 
