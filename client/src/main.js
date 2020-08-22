492c6a2e46c7ef7a93c4f5e4c754a16ae7d619c4
Bruno Haveroth <bruno.haveroth@gmail.com>
2020-08-21 21:52:43 -0300

    Configure modules

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import 'sweetalert2/dist/sweetalert2.min.css';
 
+axios.defaults.baseURL = 'http://localhost:3000';
+
+Vue.use(VueSweetalert2);
 Vue.config.productionTip = false
 
 new Vue({
