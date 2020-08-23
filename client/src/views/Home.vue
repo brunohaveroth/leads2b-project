cbc73f2ef68c1041fbc497853be88fe742a6339a
Bruno Haveroth <bruno.haveroth@gmail.com>
2020-08-23 16:38:27 -0300

    Update welcome page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="home">
-    <h1 class="text-primary">Bem-vindo ao desafio Fullstack!</h1>
-    <h5>Pera aí, antes de qualquer coisa eu vou explicar um pouco sobre o projeto!</h5>
+    <div class="card shadow mb-4">
+      <div class="card-body">
+        <h1 class="text-primary text-center mb-5">Bem-vindo ao desafio Fullstack!</h1>
+        <h5>Pera aí, antes de qualquer coisa eu vou explicar um pouco sobre o projeto!</h5>
 
-    <h2 class="text-primary mt-4">O que foi feito?</h2>
-    <h5>Este sistema permite realizar o cadastro de
-      <router-link class="text-primary" to="/employee">COLABORADORES</router-link> e
-      <router-link class="text-primary" to="/projects">PROJETOS</router-link>.
-    </h5>
+        <h2 class="text-primary mt-4">Cadastro de colaborador</h2>
+        <h5>
+          Ao cadastrar um colaborador é possível informar todas as competências profissionais do mesmo, como por exemplo o nível de conhecimento em node.js, vue.js, Photoshop e etc.
+        </h5>
+        <h5>
+          Durante as etapas de cadastro também é possível adicionar uma   escala de 1 até 10 ao nível de conhecimento do colaborador em cada competência profissional informada.
+        </h5>
 
-    <h2 class="text-primary mt-4">As competências</h2>
-    <h5>
-      Ao cadastrar um colaborador é possível informar todas as competências profissionais do mesmo, como por exemplo o nível de conhecimento em node.js, vue.js, Photoshop e etc.
-    </h5>
-    <h5>
-      Durante as etapas de cadastro também é possível adicionar uma   escala de 1 até 10 ao nível de conhecimento do colaborador em cada competência profissional informada.
-    </h5>
-    <h5>
-      No cadastro de projetos também é possível informar quais competências os profissionais participantes devem possuir. Desta forma, o sistema dará sugestões de colaboradores ideais na hora de adicionar participantes no projeto.
-    </h5>
+        <img class="border shadow-sm mw-100 mt-3" src="../assets/images/preview-1.png" alt="Preview 1">
+
+        <h2 class="text-primary mt-4">Cadastro de projetos</h2>
+        <h5>
+          No cadastro de projetos também é possível informar quais competências os profissionais participantes devem possuir.
+        </h5>
+        <img class="border shadow-sm mw-100 mt-3" src="../assets/images/preview-2.png" alt="Preview 2">
+        <h5 class="mt-5">
+          Desta forma, o sistema dará sugestões de colaboradores ideais na hora de adicionar participantes no projeto.
+        </h5>
+        <img class="border shadow-sm mw-100 mt-3" src="../assets/images/preview-3.png" alt="Preview 3">
+
+        <h5 class="mt-5 text-muted">
+          Quando você criou esta conta, o sistema gerou alguns registros automaticamente para melhorar a sua experiência.
+        </h5>
+      </div>
+    </div>
   </div>
 </template>
